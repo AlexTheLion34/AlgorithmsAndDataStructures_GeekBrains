a6653c2ed0c34f1c2a3ec46fb3d4bb130dbdb2e6
Alexey Petrenko <AlexTheLion@MacBook-AirAleksej.local>
2018-07-23 21:32:48 +0200

    Fourth homework done

---
+++ Homework_4_Alg/src/Element.java
@@ -0,0 +1,28 @@
+public class Element {
+
+    private final int value;
+    private Element nextElement;
+
+    public Element(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Element getNextElement() {
+        return nextElement;
+    }
+
+    public void setNextElement(Element nextElement) {
+        this.nextElement = nextElement;
+    }
+
+    @Override
+    public String toString() {
+        return "Element{" +
+                "value=" + value +
+                '}';
+    }
+}
